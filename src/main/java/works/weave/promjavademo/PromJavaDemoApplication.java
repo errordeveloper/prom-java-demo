5989e88f8c40c46a2c8827f32afa7dbb3ea43b93
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-09-21 14:27:49 +0100

    Create the project with Spring Initializr

---
+++ src/main/java/works/weave/promjavademo/PromJavaDemoApplication.java
@@ -0,0 +1,12 @@
+package works.weave.promjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromJavaDemoApplication.class, args);
+	}
+}
