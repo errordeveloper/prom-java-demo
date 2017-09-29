50a50acf3c2a5e46f7f12a62cda972e3a00f4c2a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-09-29 16:14:16 -0700

    Minor cleanup

--- src/main/java/works/weave/promjavademo/PromJavaDemoApplication.java
+++ src/main/java/works/weave/promjavademo/PromJavaDemoApplication.java
@@ -2,7 +2,6 @@ package works.weave.promjavademo;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
